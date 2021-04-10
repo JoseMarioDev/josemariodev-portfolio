2aa1187b43473d11777e77e5152fbe65883e4b68
JoseMarioDev <josemariodev@gmail.com>
2021-04-10 09:34:32 -0500

    :sparkles: Creating Intro component and importing it

---
+++ src/components/intro/Intro.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import selfie from '../../assets/selfie-10.jpg';
+const Intro = () => {
+  return (
+    <section className="intro" id="home">
+      <h1 className="section__title section__title--intro">
+        Hi, I am <strong>Jose Mario</strong>
+      </h1>
+      <p className="section__subtitle section__subtitle--intro">
+        Full-Stack Developer
+      </p>
+      <img src={selfie} alt="Jose red selfie" className="intro__img" />
+    </section>
+  );
+};
+
+export default Intro;
