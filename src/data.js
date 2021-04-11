af9485ec853b2333bcb90b9acfefe583741dfdde
josemariodev <josemariodev@gmail.com>
2021-04-10 22:52:17 -0500

    :recycle: Mapping over skills, rendering in Skills component

--- src/data.js
+++ src/data.js
@@ -23,6 +23,84 @@ export const socials = [
     title: 'Twitter'
   }
 ];
+export const skills = [
+  {
+    header: 'Design',
+    body:
+      'I like using modern design tools to create intuitive design patterns and thoughtful interactions.',
+    icons: [
+      {
+        icon: 'fab fa-figma',
+        title: 'Figma'
+      },
+      {
+        icon: 'fab fa-sketch',
+        title: 'Sketch'
+      },
+      {
+        icon: 'fab fa-adobe',
+        title: 'Adobe'
+      }
+    ]
+  },
+  {
+    header: 'Front-End',
+    body:
+      'I write clean, modular components using popular frameworks that are accessible across all devices.',
+    icons: [
+      {
+        icon: 'fab fa-js',
+        title: 'JavaScript'
+      },
+      {
+        icon: 'fab fa-react',
+        title: 'React'
+      },
+      {
+        icon: 'fas fa-mobile-alt',
+        title: 'Responsive'
+      }
+    ]
+  },
+  {
+    header: 'Back-End',
+    body:
+      'I design databases and optimize queries. I am familiar with testing and writing documentation.',
+    icons: [
+      {
+        icon: 'fab fa-node-js',
+        title: 'NodeJS'
+      },
+      {
+        icon: 'fab fa-python',
+        title: 'Python'
+      },
+      {
+        icon: 'fab fa-git-alt',
+        title: 'Git'
+      }
+    ]
+  },
+  {
+    header: 'Collaboration',
+    body:
+      'I genuinely care about people, and love helping fellow developers work on their craft.',
+    icons: [
+      {
+        icon: 'fab fa-trello',
+        title: 'Trello'
+      },
+      {
+        icon: 'fas fa-sync',
+        title: 'Agile'
+      },
+      {
+        icon: 'fab fa-slack',
+        title: 'Slack'
+      }
+    ]
+  }
+];
 
 export const about = [
   {
