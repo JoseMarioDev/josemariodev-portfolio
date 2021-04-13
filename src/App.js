7221ca586c8b21aa6dd7f0b9a1a830efbe9fb631
JoseMarioDev <josemariodev@gmail.com>
2021-04-13 08:22:04 -0500

    :art: adding routes to app.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,20 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './scss/main.scss';
 
 import Header from './components/header/Header';
+import About from './components/about/About';
 import Home from './components/home/Home';
 import PortfolioItem from './components/portfolio/PortfolioItem';
+import Skills from './components/skills/Skills';
+import Portfolio from './components/portfolio/Portfolio';
 
 function App() {
   return (
     <Router>
       <Header />
       <Route exact path="/" render={() => <Home />} />
+      <Route exact path="/skills" render={() => <Skills />} />
+      <Route exact path="/about" render={() => <About />} />
+      <Route exact path="/portfolio" render={() => <Portfolio />} />
       <Route exact path="/portfolio/:id" render={() => <PortfolioItem />} />
     </Router>
   );
