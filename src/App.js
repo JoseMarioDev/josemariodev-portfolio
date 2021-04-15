ff5d2fda996a9a6247091eda8605a3da27c118b0
JoseMarioDev <josemariodev@gmail.com>
2021-04-15 17:01:31 -0500

    :sparkles: Creating ScrollToTop func

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
 
 import './scss/main.scss';
-
+import ScrollToTop from './utils/ScrollToTop';
 import Header from './components/header/Header';
 import About from './components/about/About';
 import Home from './components/home/Home';
@@ -27,14 +27,16 @@ const App = ({ location }) => {
   return (
     <div>
       {routeArray.includes(location.pathname) && <Header />}
-      <Switch>
-        <Route exact path="/" render={() => <Home />} />
-        <Route exact path="/skills" render={() => <Skills />} />
-        <Route exact path="/about" render={() => <About />} />
-        <Route exact path="/portfolio" render={() => <Portfolio />} />
-        <Route exact path="/portfolio/:id" render={() => <PortfolioItem />} />
-        <Route path="*" render={() => <ErrorPage />} />
-      </Switch>
+      <ScrollToTop>
+        <Switch>
+          <Route exact path="/" render={() => <Home />} />
+          <Route exact path="/skills" render={() => <Skills />} />
+          <Route exact path="/about" render={() => <About />} />
+          <Route exact path="/portfolio" render={() => <Portfolio />} />
+          <Route exact path="/portfolio/:id" render={() => <PortfolioItem />} />
+          <Route path="*" render={() => <ErrorPage />} />
+        </Switch>
+      </ScrollToTop>
       {routeArray.includes(location.pathname) && <Footer />}
     </div>
   );
---
+++ src/utils/ScrollToTop.js
@@ -0,0 +1,21 @@
+import React, { useEffect, Fragment } from 'react';
+import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const ScrollToTop = ({ history, children }) => {
+  useEffect(() => {
+    const unlisten = history.listen(() => {
+      window.scrollTo(0, 0);
+    });
+    return () => {
+      unlisten();
+    };
+  }, []);
+
+  return <Fragment>{children}</Fragment>;
+};
+ScrollToTop.propTypes = {
+  history: PropTypes.object,
+  children: PropTypes.object
+};
+export default withRouter(ScrollToTop);
