0bb6fcf9c2dae1c03cafb83abfbfcdc621b99f78
JoseMarioDev <josemariodev@gmail.com>
2021-04-19 14:45:50 -0500

    :green_heart: Adding dynamo config to tf

---
+++ infrastructure/dynamo.tf
@@ -0,0 +1,29 @@
+#################
+# DYNAMODB RESOURCES
+#################
+# table
+
+resource "aws_dynamodb_table" "VisitorTable" {
+  hash_key     = "Site"
+  name         = "VisitorTable"
+  billing_mode = "PAY_PER_REQUEST"
+  attribute {
+    name = "Site"
+    type = "S"
+  }
+}
+
+# add first item but only the first time run
+
+resource "aws_dynamodb_table_item" "VisitorTableItem" {
+  table_name = aws_dynamodb_table.VisitorTable.name
+  hash_key   = aws_dynamodb_table.VisitorTable.hash_key
+  lifecycle { ignore_changes = [item] }
+
+  item = <<ITEM
+{
+  "Site": {"S": "Resume"},
+  "Visitors": {"N": "0"}
+}
+ITEM
+}
